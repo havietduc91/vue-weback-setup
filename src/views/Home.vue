e7bf81109c52d6360c2c3d9aa70b01dbd859f497
Hà Việt Đức <havietduc91@gmail.com>
2021-07-16 14:29:15 +0700

    Fix bug to npm run start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,6 @@
 <template>
   <div id="home">
-    <!-- <img class="logo" src="../assets/logo.png" alt="logo" /> -->
+    <img class="logo" src="../assets/logo.png" alt="logo" />
     <h1>👋Hello world🌎</h1>
   </div>
 </template>
-
-
-<script>
-export default {
-  name: "Home.vue"
-}
-</script>
-
-<style scoped>
-
-</style>
